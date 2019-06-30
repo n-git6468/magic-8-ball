4bb977c8000f1d510384f136eae7667471afbeb9
nhwh <noahwhalen@lavabit.com>
2019-06-30 12:59:25 -0400

    Refactored

--- src/magic_8_ball/magic_8_ball.clj
+++ src/magic_8_ball/magic_8_ball.clj
@@ -2,5 +2,7 @@
 
 
 
+(def potential-fortunes ["It is certain!" "It is decidedly so!" "Yes" "Reply hazy try again" "Concentrate & ask again!" "My reply is no" "Outlook is not so good" "Very doubtful!"])
+
 (defn fortunes []
-  (->> ["It is certain!" "It is decidedly so!" "Yes" "Reply hazy try again" "Concentrate & ask again!" "My reply is no" "Outlook is not so good" "Very doubtful!"] count rand-int (nth ["It is certain!" "It is decidedly so!" "Yes" "Reply hazy try again" "Concentrate & ask again!" "My reply is no" "Outlook is not so good" "Very doubtful!"]) print))
+  (->> potential-fortunes count rand-int (nth potential-fortunes) print))
