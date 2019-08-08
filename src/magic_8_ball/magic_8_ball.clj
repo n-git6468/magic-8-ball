aefa9cf2006fa599e552b80d3c20adcfe9361bc8
noah <noah@pop-os.localdomain>
2019-08-08 14:14:41 -0400

    Updated potential-futures formatting. Fixed punctuation.

--- src/magic_8_ball/magic_8_ball.clj
+++ src/magic_8_ball/magic_8_ball.clj
@@ -2,7 +2,15 @@
 
 
 
-(def potential-fortunes ["It is certain!" "It is decidedly so!" "Yes" "Reply hazy try again" "Concentrate & ask again!" "My reply is no" "Outlook is not so good" "Very doubtful!"])
+(def potential-fortunes
+  ["It is certain!"
+   "It is decidedly so!"
+   "Yes!"
+   "Reply hazy! Try again!"
+   "Concentrate & ask again!"
+   "My reply is no"
+   "Outlook is not so good!"
+   "Very doubtful!"])
 
 (defn fortunes []
   (->> potential-fortunes count rand-int (nth potential-fortunes) print))
